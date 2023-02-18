710f2b60eeef7f5e855cebbe4a69b2ca2a3ca7cd
boden <78735871+bodeneinheit@users.noreply.github.com>
2023-02-18 23:01:54 +0100

    Update pubstomping.js

--- pubstomping.js
+++ pubstomping.js
@@ -1,20 +1,21 @@
 //params
 //make sure kill, death and streak counters are enabled
-const autoLogInOut = true; //true = autologs in and out, false does not
-const selectedModes = false; //true = leaves if gamemodes is not in the array below
-const gameModes = ["ffa", "shrp", "krank", "bhffa", "depoffa", "chs"]; //gamemodes as array
+const autoLogInOut = false; //true = autologs in and out, false does not
+const selectedModes = true; //true = leaves if gamemodes is not in the array below
+const gameModes = ["ffa", "shrp", "krank", "depoffa", "chs", "tdm"]; //gamemodes as array
 const autoPick = true; // true = autopicks class below - !!autoloading the loadout presets will override this!!
 const weaponClass = 0; // pick value from below
 const autoLoadLoadout = true; //true = will automatically equip/load the loadout entered below
-const loadout = "lol"; //change to the loadout you want to have equipped on gameload
+const loadout = "drip"; //change to the loadout you want to have equipped on gameload
 //0 = ak, 1 = sniper, 2 = smg, 3 = lmg, 4 = shotgun, 5 = rev, 6 = semi, 7 = rpg
 //8 = uzi, 9 = runner, 11 = cbow, 12 = famas, 13 = trooper, 15 = charge rifle
 
-const leaveOnDeath = true; // true = leaves when dying; false = doesnt leave when dying
-const leaveOnLowTime = false // true = leaves once time is lower than the one entered in the line below
-const leaveTime = 100; // enter time to leave at (in seconds) when no kills were made  - set to a negative value to never leave
+const leaveOnDeath = false; // true = leaves when dying; false = doesnt leave when dying
+const leaveOnLowTime = false; // true = leaves once time is lower than the one entered in the line below
+const leaveTime = 10; // enter time to leave at (in seconds) when no kills were made  - set to a negative value to never leave
 const stayNuke = true; // true = always stay when nuked
 const stayDoubleNuke = true; // true = always stay when doublenuked
+const minimumKDR = 5; //minimum required kdr
 
 /*----------------------------------------------------------------------------------*/
 //dont touch anything below here
@@ -38,6 +39,9 @@ setInterval(() => {
     //count kills
     const kills = document.getElementById("killsVal").innerText;
 
+    //calcualte kd
+    const killperdeath = kills / deaths;
+
     //checks for nuke
     if (streak == '25') nuke = true;
     //checks for doublenuke
@@ -45,7 +49,7 @@ setInterval(() => {
     //checks for nukes
     if (!(stayNuke && nuke) && !(stayDoubleNuke && doubleNuke)) {
         //checks for deaths and lowtime
-        if ((leaveOnDeath && deaths == '1') || (leaveOnLowTime && kills == 0 && leavetime > lobbyRestTime)) {
+        if ((leaveOnDeath && deaths == '1') || (leaveOnLowTime && kills == 0 && leavetime > lobbyRestTime) || (minimumKDR >= killperdeath)) {
             //logout
             if (autoLogInOut) logoutAcc();
             //open krunker
