488b822987740164d064abab1f9714bda674eca4
boden <78735871+bodeneinheit@users.noreply.github.com>
2023-02-28 21:31:58 +0100

    Update f4.js

--- f4.js
+++ f4.js
@@ -1,6 +1,5 @@
 document.addEventListener('keydown', (ev) => {
     if (ev.keyCode === 115) {
-        window.logoutAcc();
         setTimeout(() => {
             window.location = 'https://krunker.io'
         }, 100)
